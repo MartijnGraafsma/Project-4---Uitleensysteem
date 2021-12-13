b953366f63131ec1e9a714be894a146d0b8a4f79
MartijnGraafsma <91067234+MartijnGraafsma@users.noreply.github.com>
2021-12-13 15:16:01 +0100

    Update gebruikertoevoegen.php

--- gebruikertoevoegen.php
+++ gebruikertoevoegen.php
@@ -13,33 +13,60 @@
         <h3 class="gebruiker_tekst">Gebruiker Toevoegen</h3>
         <form method="post" action="#">
             <input type="txt" name="naam" class="nieuwe_user" placeholder="Naam">
+
             <input type="txt" name="gebruikersnaam"class="nieuwe_user" placeholder="Gebruikersnaam">
+
             <input type="txt" name="wachtwoord" class="nieuwe_user" placeholder="Wachtwoord">
+
             <button type="submit" class="voeg_toe">Voeg Toe</button>
         </form>
 
     </div>
+    <div class="accounts_container">
+        <form method="post" action="#">
+          <div class="zoek_container"><input name="search" class="nieuwe_user" placeholder="Zoeken"></div>
+
+
+        </form>
+    </div>
+
+
 </body>
 </html>
-
 <?php
 include "config.php";
 
-$sql = "SELECT * FROM `users`";
+if ($conn -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $conn -> connect_error;
+  exit();
+}
 
+$sql ="SELECT * From `users`";
 
 echo "
 <table class='content-table'>
 <thead>
   <tr>
     <th>ID</th>
-    <th>Aantal</th>
     <th>Naam</th>
-    <th>Adres</th>
-    <th>E-mail</th>
-    <th>Telefoonnummer</th>
-    <th>Status</th>
-    <th>Delete</th>
+    <th>Gebruikersnaam</th>
+    <th>Wachtwoord</th>
+    
   </tr>
 </thead>";
+
+if ($result = $conn->query($sql)) {
+  foreach ($result as $row) {
+  echo "
+
+  <tr>
+      <td>" . $row['id'] . "</td> 
+      <td>" . $row['naam'] . "</td>
+      <td>" . $row['gebruikersnaam'] . "</td>
+      <td>" . $row['wachtwoord'] . "</td>";
+  }
+}
+?>
+
+
 
