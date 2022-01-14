8a460d4f59fbbe865c34d891f0312b68f3b92187
MartijnGraafsma <91067234+MartijnGraafsma@users.noreply.github.com>
2022-01-14 22:49:14 +0100

    32

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 <?php
 
 $host="localhost";
-$user="root";
-$password="";
-$db="registratiesysteem";
+$user="deb85590_p22t3";
+$password="zNuMsh7iP1";
+$db="deb85590_p22t3";
 
 $conn=new mysqli($host, $user, $password, $db);
 
