ee180614f2feee49760e3ecaf1f1292b3030ea23
Shibin0613 <220617@edu.rocfriesepoort.nl>
2022-01-14 13:11:52 +0100

    Update deviceuitlenen.php

--- deviceuitlenen.php
+++ deviceuitlenen.php
@@ -9,11 +9,12 @@ $inleverdatum=$_GET['inleverdatum'];
 $email=$_GET['email'];
 $van=$_GET['van'];
 $uitgeleend=$_GET['uitgeleend'];
+$opmerking=$_GET['opmerking'];
 ?>
 
 <!DOCTYPE html>
 <head>
-    <link rel="stylesheet" href="deviceuitlenen.css">
+    <link rel="stylesheet" href="deviceleen.css">
     <link rel="stylesheet" href="header.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
 </head>
@@ -26,8 +27,10 @@ $uitgeleend=$_GET['uitgeleend'];
     </div>
       <div class="navbar">
         <ul>
-          <a href="deviceinlever.php"><li>Apparaten inleveren</li></a>
-          <a href="uitloggen.php"><li>Uitloggen</li></a>
+        <a href="apparaatoverzicht(docent).php"><li>Apparatenoverzicht</li></a>
+          <a href="apparatentoevoegen.php"><li>Apparaten toevoegen</li></a>
+          <a href="gebruikertoevoegen.php"><li>Gebruiker toevoegen</li></a>
+          <a href="inloggen.php"><li>Uitloggen</li></a>
           <a><li></li></a>
         </ul>
       <script>
@@ -43,28 +46,52 @@ $uitgeleend=$_GET['uitgeleend'];
       </div>
   </div>
 </div>    
+
 <div class="container">
 <h2 class="top-tekst">Uitlenen</h2>
-      <form action="" method="GET" class="column, ">
-         <br>
-         
-    
-         <input  class="apparaatnaam" type="text" name="van" placeholder="Van" required>
-         <br>
-         <input   class="apparaatnaam" type="text" name="uitgeleend" placeholder="Uitgeleend aan" required>
-         <br>
-         <input   class="apparaatnaam" type="email" name="email" placeholder="Email" required>
-         <br>
-         <input  class="apparaatnaam" type="text" name="productnaam" placeholder="productnaam" required>
-         <br>
-         <input  class="apparaatnaam" type="date" name="inleverdatum" placeholder="Inleverdatum" required>
-         <br>
-         <button class="voeg-toe" type="submit" name="submit">Leen uit</button>
+      <form action="mail.php" method="POST" class="column, ">
+        <input name="id" value="<?php echo "$id" ?>" hidden>
+        <input name="productnaam" value="<?php echo "$productnaam" ?>" hidden>
+        <input name="beschikbaarheid" value="Uitgeleend" hidden>
+        <input name="beschrijving" value="<?php echo "$beschrijving" ?>" hidden>
+        <br>
+        <input class="apparaatnaam" type="text" name="van" value="<?php echo "$van" ?>" placeholder="Van" required>
+        <br>
+        <input class="apparaatnaam" type="text" name="uitgeleend" value="<?php echo "$uitgeleend" ?>" placeholder="Uitgeleend aan" required>
+        <br>
+        <input class="apparaatnaam" type="email" name="email" value="<?php echo "$email" ?>" placeholder="Email" required>
+        <br>
+        <input class="apparaatnaam" type="date" name="inleverdatum" placeholder="Inleverdatum" required>
+        <br>
+        <button class="voeg-toe" type="submit" name="submit">Leen uit</button>
       </form>
-    </div>
+</div>
 <div class="cat-container">  
     <a class="top-tekst2"><?php echo "$productnaam" ?></a>
+    <br>
+    <br>
+    <br>
+    <br>
     <a><?php echo "$beschrijving" ?></a>
+</div>
+
+<div class="inlever-container">
+<h2 class="top-tekst">INLEVER</h2>
+      <form action="inlever.php" method="POST" class="column, ">
+        <input name="id" value="<?php echo "$id" ?>" hidden>
+        <input name="productnaam" value="<?php echo "$productnaam" ?>" hidden>
+        <input name="beschikbaarheid" value="Beschikbaar" hidden>
+        <input name="beschrijving" value="<?php echo "$beschrijving" ?>" hidden> 
+        <input name="van" value="" hidden>
+        <input name="email" value="" hidden>
+        <input name="inleverdatum" value="/" hidden>
+        <br>
+        <input class="apparaatnaam" type="text" name="uitgeleend" placeholder="Uitgeleend aan" value="<?php echo "$uitgeleend" ?>" required>
+        <br>
+        <input class="apparaatnaam" type="text" name="opmerking" placeholder="Nieuwe opmerking" required>
+        <a class="apparaatnaam"><?php echo "$opmerking" ?></a>
+        <button class="voeg-toe" type="submit" name="submit">Inleveren</button>
+      </form>
 </div>
     
 
@@ -72,29 +99,5 @@ $uitgeleend=$_GET['uitgeleend'];
 
 </body>   
 </html>
-    <?php
-      if($_GET['submit']){
-        $id=$_GET['id'];
-        $productnaam=$_GET['productnaam'];
-        $beschrijving=$_GET['beschrijving'];
-        $inleverdatum=$_GET['inleverdatum'];
-        $email=$_GET['email'];
-        $van=$_GET['van'];
-        $uitgeleend=$_GET['uitgeleend'];
-        $sql = "UPDATE apparaatoverzicht SET productnaam='[$productnaam]',beschrijving='$beschrijving'
-        ,inleverdatum='$inleverdatum',email='$email',van='$van',
-         uitgeleend='$uitgeleend' WHERE id='$id'";
-  
-        $data = mysqli_query($conn,$sql);
-      if($data){
-        echo "<script>alert('gegevens opgeslagen')</script>";
-        ?>
-        <META HTTP-EQUIV="Refresh" CONTENT="0; URL=http://localhost/systeem/apparaatoverzicht(docent).php">
-        <?php
-      }else{
-        echo "<script>alert('Het is niet gelukt om uw gegevens te wijzigen, Probeer later opnieuw')</script>";
-      }
-      }
 
-    ?>
 
